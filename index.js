b82c53f9d94deec7a48de22ef6f2e9acda2e70c4
Miguel Patrocinio <jose.m.patrocinio@hotmail.com>
2020-04-27 15:58:42 +0200

    changing the active player

--- index.js
+++ index.js
@@ -14,11 +14,18 @@ let activePlayer = 0;
 // let dice = Math.floor(Math.random() * 6) + 1;
 // console.log(dice);
 
-document.querySelector("#current-" + activePlayer).textContent = dice;
+// document.querySelector("#current-" + activePlayer).textContent = dice;
 // document.querySelector("#current-" + activePlayer).innerHTML =
 //   "<p>" +
 var x = document.querySelector("#score-0").textContent;
 console.log(x);
+
+//////////////////////////////////////////////
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
 document.querySelector(".dice").style.display = "none";
 
@@ -32,4 +39,27 @@ document.querySelector(".btn-roll").addEventListener("click", () => {
   diceDOM.src = "./images/dice-" + dice + ".png";
 
   // 3 update the round score if the rolled number was not 1
+
+  if (dice !== 1) {
+    //add score
+    roundScores += dice;
+    document.querySelector(
+      "#current-" + activePlayer
+    ).textContent = roundScores;
+  } else {
+    //next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScores = 0;
+
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    // document.querySelector(".player-0-panel").classList.remove("active");
+    // document.querySelector(".player-1-panel").classList.add("active");
+
+    document.querySelector(".dice").style.display = "none";
+  }
 });
