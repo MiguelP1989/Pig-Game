c648b9da01014a1714ca464700dce2f592c55301
Miguel Patrocinio <jose.m.patrocinio@hotmail.com>
2020-04-28 12:01:13 +0200

    creating game initialization function

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-let scores = [0, 0];
-let roundScores = 0;
-let activePlayer = 0;
+let scores;
+let roundScores;
+let activePlayer;
+
+init();
 
 // let dice = Math.floor(Math.random() * 6) + 1;
 // console.log(dice);
@@ -17,17 +19,8 @@ let activePlayer = 0;
 // document.querySelector("#current-" + activePlayer).textContent = dice;
 // document.querySelector("#current-" + activePlayer).innerHTML =
 //   "<p>" +
-var x = document.querySelector("#score-0").textContent;
-console.log(x);
 
 //////////////////////////////////////////////
-
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
-
-document.querySelector(".dice").style.display = "none";
 
 document.querySelector(".btn-roll").addEventListener("click", () => {
   // 1 random number
@@ -94,4 +87,27 @@ function nextPlayer() {
   // document.querySelector(".player-1-panel").classList.add("active");
 
   document.querySelector(".dice").style.display = "none";
+}
+
+document.querySelector(".btn-new").addEventListener("click", init);
+
+function init() {
+  scores = [0, 0];
+  roundScores = 0;
+  activePlayer = 0;
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.querySelector(".dice").style.display = "none";
+
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
 }
